c18789a771b1b4480e3e99abb91f0e175edbc7aa
teguhpermadi <tguh.permadi@gmail.com>
2023-09-02 20:57:30 +0700

    academic year

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TeacherSeeder::class,
             StudentSeeder::class,
+            AcademicYearSeeder::class,
         ]);
     }
 }
