c33cf1353db79b6acd433270e841f3643adb782e
teguhpermadi <tguh.permadi@gmail.com>
2025-12-31 17:23:33 +0700

    feat: Menambahkan fungsionalitas impor data siswa dari Excel.

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -30,51 +30,53 @@ class StudentImport implements ToCollection, WithHeadingRow
         foreach ($rows as $index => $row) {
             $currentRow = $index + 2; // +2 karena heading di baris 1 dan index mulai dari 0
             try {
-                $student = Student::create([
-                    'nisn' => $row['nisn'],
-                    'nis' => $row['nis'],
-                    'name' => $row['nama_lengkap'],
-                    'nick_name' => $row['nama_panggilan'],
-                    'gender' => Str::lower($row['jenis_kelamin']),
-                    'city_born' => $row['tempat_lahir'],
-                    'birthday' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal_lahir'], 'Asia/Jakarta'),
-                ]);
+                $student = Student::updateOrCreate(
+                    ['nis' => $row['nis']],
+                    [
+                        'nisn' => $row['nisn'],
+                        'name' => $row['nama_lengkap'],
+                        'nick_name' => $row['nama_panggilan'],
+                        'gender' => Str::lower($row['jenis_kelamin']),
+                        'city_born' => $row['tempat_lahir'],
+                        'birthday' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal_lahir'], 'Asia/Jakarta'),
+                    ]
+                );
 
-                DataStudent::create([
-                    'student_id' => $student->id,
-                    'student_address' => $row['alamat_siswa'],
-                    'student_province' => $row['provinsi_siswa'],
-                    'student_city' => $row['kota_siswa'],
-                    'student_district' => $row['kecamatan_siswa'],
-                    'student_village' => $row['kelurahan_siswa'],
-                    'religion' => $row['agama'],
-                    'previous_school' => $row['asal_sekolah'],
-                    'date_received' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal_diterima'], 'Asia/Jakarta'),
-                    'grade_received' => $row['diterima_kelas'],
-                    'father_name' => $row['nama_ayah'],
-                    'father_education' => $row['pendidikan_ayah'],
-                    'father_occupation' => $row['pekerjaan_ayah'],
-                    'father_phone' => $row['telp_ayah'],
-                    'mother_name' => $row['nama_ibu'],
-                    'mother_education' => $row['pendidikan_ibu'],
-                    'mother_occupation' => $row['pekerjaan_ibu'],
-                    'mother_phone' => $row['telp_ibu'],
-                    'guardian_name' => $row['nama_wali'],
-                    'guardian_education' => $row['pendidikan_wali'],
-                    'guardian_occupation' => $row['pekerjaan_wali'],
-                    'guardian_phone' => $row['telp_wali'],
-                    'guardian_address' => $row['alamat_wali'],
-                    // 'guardian_village' => $row['kelurahan_wali'],
-                    'parent_address' => $row['alamat_orangtua'],
-                    'parent_village' => $row['kelurahan_orangtua'],
-                    'parent_address' => $row['alamat_orangtua'],
-                    'parent_province' => $row['provinsi_orangtua'],
-                    'parent_city' => $row['kota_orangtua'],
-                    'parent_district' => $row['kecamatan_orangtua'],
-                    'parent_village' => $row['kelurahan_orangtua'],
-                    'height' => $row['tinggi_badan'],
-                    'weight' => $row['berat_badan'],
-                ]);
+                DataStudent::updateOrCreate(
+                    ['student_id' => $student->id],
+                    [
+                        'student_address' => $row['alamat_siswa'],
+                        'student_province' => $row['provinsi_siswa'],
+                        'student_city' => $row['kota_siswa'],
+                        'student_district' => $row['kecamatan_siswa'],
+                        'student_village' => $row['kelurahan_siswa'],
+                        'religion' => $row['agama'],
+                        'previous_school' => $row['asal_sekolah'],
+                        'date_received' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal_diterima'], 'Asia/Jakarta'),
+                        'grade_received' => $row['diterima_kelas'],
+                        'father_name' => $row['nama_ayah'],
+                        'father_education' => $row['pendidikan_ayah'],
+                        'father_occupation' => $row['pekerjaan_ayah'],
+                        'father_phone' => $row['telp_ayah'],
+                        'mother_name' => $row['nama_ibu'],
+                        'mother_education' => $row['pendidikan_ibu'],
+                        'mother_occupation' => $row['pekerjaan_ibu'],
+                        'mother_phone' => $row['telp_ibu'],
+                        'guardian_name' => $row['nama_wali'],
+                        'guardian_education' => $row['pendidikan_wali'],
+                        'guardian_occupation' => $row['pekerjaan_wali'],
+                        'guardian_phone' => $row['telp_wali'],
+                        'guardian_address' => $row['alamat_wali'],
+                        // 'guardian_village' => $row['kelurahan_wali'],
+                        'parent_address' => $row['alamat_orangtua'],
+                        'parent_village' => $row['kelurahan_orangtua'],
+                        'parent_province' => $row['provinsi_orangtua'],
+                        'parent_city' => $row['kota_orangtua'],
+                        'parent_district' => $row['kecamatan_orangtua'],
+                        'height' => $row['tinggi_badan'],
+                        'weight' => $row['berat_badan'],
+                    ]
+                );
 
                 $identifier = $row['nisn'] ?: $row['nama_lengkap'];
                 Log::info("Student Import Success: {$identifier} on row {$currentRow}");
